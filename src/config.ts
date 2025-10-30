dd7789aa5ddd69f3e46ed92219a4c190e7c88445
Enrique <hein@cherrybomb.com.mx>
2025-10-29 23:56:20 -0600

    Add more works and fix meta tags

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Enrique Mendoza — Frontend Developer';
-export const SITE_DESCRIPTION = "Hello, I'm a passionate web developer creating web-based solutions since 2010, relying on good practices and an adequate stack of technologies.";
+export const SITE_DESCRIPTION = "Hello, I'm a web developer crafting web-based solutions since 2010, relying on good practices and state-of-the-art technologies.";
 // export const GENERATE_SLUG_FROM_TITLE = true
 // export const TRANSITION_API = true
